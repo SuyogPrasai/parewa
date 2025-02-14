aee26192fa9aa1ce1d9d0d5945aacb89da2fe201
suyog prasai <suyog224356@gmail.com>
2025-02-14 09:25:33 +0000

    added proper ui for articles

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["source.unsplash.com"], // Add the domain here
-  },
 };
 
 export default nextConfig;
