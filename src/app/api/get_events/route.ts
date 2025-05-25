9857d5b89a750e1442b88961c2bc790a33d3951c
suyog prasai <suyog224356@gmail.com>
2025-05-25 06:57:03 +0000

    the events api works

---
+++ src/app/api/get_events/route.ts
@@ -0,0 +1,35 @@
+import dbConnect from "@/lib/dbConnnect";
+
+import { NextResponse, NextRequest } from "next/server";
+import EventModel from "@/models/Event";
+import UserModel, { User } from "@/models/User"; // Assuming you have a User model
+
+import { Types } from "mongoose"; // Import Mongoose types
+
+export async function GET(request: NextRequest) {
+    await dbConnect();
+
+    const { searchParams } = new URL(request.url);
+    let date = searchParams.get("date");
+
+    if (!date) {
+        console.log("Date is required.");
+        return NextResponse.json({ success: false, message: "Date is required" }, { status: 400 });
+    }
+
+    const events = await EventModel.findOne({ start_date: date });
+
+    try {
+    
+        return NextResponse.json(
+            { success: true, events: events},
+            { status: 200 }
+        );
+    } catch (error: any) {
+        console.error("Error fetching notices from the database:", error);
+        return NextResponse.json(
+            { success: false, message: "Error fetching notices from the server" },
+            { status: 400 }
+        );
+    }
+}
---
+++ src/models/Event.tsx
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Defining the interface for the Notice model
+export interface Event extends Document {
+    title: string;
+    start_date: Date;
+    end_date: Date;
+}
+
+// Defining the Notice schema
+const EventSchema: Schema<Event> = new Schema(
+    {
+        title: { type: String, required: true, trim: true },
+        start_date: { type: Date, required: true },
+        end_date: { type: Date, required: true },
+    },
+    {
+        timestamps: true, // Adds createdAt and updatedAt fields automatically
+    }
+);
+
+// Exporting the model
+const EventModel = (mongoose.models.Event as mongoose.Model<Event>) || (mongoose.model<Event>('Event', EventSchema));
+
+export default EventModel;
